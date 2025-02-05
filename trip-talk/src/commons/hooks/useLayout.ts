85671de89ff341c2243e043093379bd5c63fb261
yheseul <kangyheseul@naver.com>
2025-02-05 23:18:01 +0900

    refactor: 상단 배너 판별 정규 표현식 및 배너 비노출 pathname 리스트 수정

--- trip-talk/src/commons/hooks/useLayout.ts
+++ trip-talk/src/commons/hooks/useLayout.ts
@@ -1,10 +1,11 @@
 import { usePathname } from "next/navigation";
 
 export default function useLayout() {
-  const HIDDEN_HEADER = ["/boards/new", "/boards"];
+  const HIDDEN_HEADER = ["/boards/purchase", "/boards"];
 
   const pathname = usePathname();
-  const isBoardDetailPage = /^\/boards\/[a-zA-Z0-9]+$/.test(pathname);
+  const isBoardDetailPage =
+    /^\/boards\/(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z0-9]+$/.test(pathname);
 
   const isHiddenHeader = HIDDEN_HEADER.includes(pathname) || isBoardDetailPage;
 
